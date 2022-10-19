0b951d95e65b1844da5871a9894884a9b8ca6c9a
Devvara Rishivian <rishivian@gmail.com>
2022-10-20 00:05:47 +0700

    save

---
+++ components/Footer.js
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { NativeBaseProvider, Box, Center, HStack, Pressable, Text } from "native-base";
+
+export default function Footer() {
+    const [selected, setSelected] = React.useState(1);
+    
+    return (
+        <NativeBaseProvider>
+            <Box flex={1} bg="white" safeAreaTop width="100%" maxW="300px" alignSelf="center">
+                <Center flex={1}></Center>
+                <HStack bg="indigo.600" alignItems="center" safeAreaBottom shadow={6}>
+                    <Pressable cursor="pointer" opacity={selected === 0 ? 1 : 0.5} py="3" flex={1} onPress={() => setSelected(0)}>
+                        <Center>\
+                            <Text color="white" fontSize="12">
+                                Home
+                            </Text>
+                        </Center>
+                    </Pressable>
+                    <Pressable cursor="pointer" opacity={selected === 1 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(1)}>
+                        <Center>
+                            <Text color="white" fontSize="12">
+                                Search
+                            </Text>
+                        </Center>
+                    </Pressable>
+                    <Pressable cursor="pointer" opacity={selected === 2 ? 1 : 0.6} py="2" flex={1} onPress={() => setSelected(2)}>
+                        <Center>
+                            <Text color="white" fontSize="12">
+                                Cart
+                            </Text>
+                        </Center>
+                    </Pressable>
+                    <Pressable cursor="pointer" opacity={selected === 3 ? 1 : 0.5} py="2" flex={1} onPress={() => setSelected(3)}>
+                        <Center>
+                            <Text color="white" fontSize="12">
+                                Account
+                            </Text>
+                        </Center>
+                    </Pressable>
+                </HStack>
+            </Box>
+        </NativeBaseProvider>
+    );
+}
--- screens/Timeline.js
+++ screens/Timeline.js
@@ -1,16 +1,16 @@
 import * as React from 'react';
 import { Button, View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Footer from '../components/Footer';
 
 export default function TimelineScreen({ navigation }) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Timeline Screen</Text>
-        <Button
-          title="Go to Landing"
-          onPress={() => navigation.navigate('Landing')}
-        />
-      </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Timeline Screen</Text>
+            <Button
+                title="Go to Landing"
+                onPress={() => navigation.navigate('Landing')}
+            />
+            <Footer></Footer>
+        </View>
     );
-  }
+}
