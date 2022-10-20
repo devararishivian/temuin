cf1cc07ccb220b97fb7248641839cf270616caee
Devvara Rishivian <rishivian@gmail.com>
2022-10-21 00:18:25 +0700

    feat: prepare auth screen

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigator from './src/components/BottomNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import useAuthStore from './src/store/index'
 import LandingScreen from './src/screens/Auth/Landing';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/Auth/Login';
+import RegisterScreen from './src/screens/Auth/Register';
 
 const Stack = createNativeStackNavigator();
 
 const AuthScreen = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Landing Screen" component={LandingScreen} options={{ title: '' }} />
+      <Stack.Screen name="Landing" component={LandingScreen} options={{ title: '' }} />
+      <Stack.Screen name="Login" component={LoginScreen} options={{ title: '' }} />
+      <Stack.Screen name="Register" component={RegisterScreen} options={{ title: '' }} />
     </Stack.Navigator>
   );
 }
--- src/screens/Auth/Landing.js
+++ src/screens/Auth/Landing.js
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 
 export default function LandingScreen({ navigation }) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Landing Screen</Text>
-        <Text>Disini nantinya akan mengarahkan ke Login / register jika belum login</Text>
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Landing Screen</Text>
+      <Text>Disini nantinya akan mengarahkan ke Login / register jika belum login</Text>
 
-        <Button
-                title="Go to Landing"
-                onPress={() => navigation.navigate('Landing')}
-            />
-      </View>
-    );
-  }
+      <Button
+        title="Mulai"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
+}
