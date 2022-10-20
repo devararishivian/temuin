0d7d2c855b873510cdd14b636b740f4bfa56b424
Devvara Rishivian <rishivian@gmail.com>
2022-10-21 00:29:05 +0700

    update

--- App.js
+++ App.js
@@ -1,21 +1,38 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import BottomNavigator from "./components/BottomNavigator";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import LoginScreen from "./screens/Login";
-import RegisterScreen from "./screens/Register";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import BottomNavigator from './src/components/BottomNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import useAuthStore from './src/store/index'
+import LandingScreen from './src/screens/Auth/Landing';
+import LoginScreen from './src/screens/Auth/Login';
+import RegisterScreen from './src/screens/Auth/Register';
 
 const Stack = createNativeStackNavigator();
 
+const AuthScreen = () => {
+  return (
+    <Stack.Navigator screenOptions={{ header: () => null }}>
+      <Stack.Screen name="Landing" component={LandingScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Register" component={RegisterScreen} />
+    </Stack.Navigator>
+  );
+}
+
+const AppScreen = () => {
+  return (
+    <BottomNavigator></BottomNavigator>
+  );
+}
+
 function App() {
+  const authData = useAuthStore(state => state.authData);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ header: () => null }}>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Register" component={RegisterScreen} />
-        </Stack.Navigator>
+        {authData ? <AppScreen /> : <AuthScreen />}
       </NavigationContainer>
     </SafeAreaProvider>
   );
