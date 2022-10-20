74405c6ff720e77dda5b288bb0917f8d1d6aa97c
routereflector <nurhakimlukman24@gmail.com>
2022-10-20 23:04:31 +0700

    base form login register

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import BottomNavigator from './components/BottomNavigator';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import BottomNavigator from "./components/BottomNavigator";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./screens/Login";
+import RegisterScreen from "./screens/Register";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <BottomNavigator></BottomNavigator>
+        <Stack.Navigator screenOptions={{ header: () => null }}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Register" component={RegisterScreen} />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
