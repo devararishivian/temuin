dc1a3a50d746e22fca70cae67f21297501884e99
Devvara Rishivian <rishivian@gmail.com>
2022-10-21 00:09:29 +0700

    feat: better routing

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomNavigator from './components/BottomNavigator';
+import BottomNavigator from './src/components/BottomNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import useAuthStore from './src/store/index'
+import LandingScreen from './src/screens/Auth/Landing';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const AuthScreen = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Landing Screen" component={LandingScreen} options={{ title: '' }} />
+    </Stack.Navigator>
+  );
+}
+
+const AppScreen = () => {
+  return (
+    <BottomNavigator></BottomNavigator>
+  );
+}
 
 function App() {
+  const authData = useAuthStore(state => state.authData);
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <BottomNavigator></BottomNavigator>
+        {authData ? <AppScreen /> : <AuthScreen />}
       </NavigationContainer>
     </SafeAreaProvider>
   );
--- src/screens/Auth/Landing.js
+++ src/screens/Auth/Landing.js
@@ -5,10 +5,12 @@ export default function LandingScreen({ navigation }) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Landing Screen</Text>
+        <Text>Disini nantinya akan mengarahkan ke Login / register jika belum login</Text>
+
         <Button
-          title="Go to Timeline"
-          onPress={() => navigation.navigate('Timeline')}
-        />
+                title="Go to Landing"
+                onPress={() => navigation.navigate('Landing')}
+            />
       </View>
     );
   }
