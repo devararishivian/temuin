347bf026ef471b6e81bf221a18a2c035bae45847
routereflector <nurhakimlukman24@gmail.com>
2022-10-21 23:43:42 +0700

    update landing page temuin

--- src/screens/Auth/Landing.js
+++ src/screens/Auth/Landing.js
@@ -1,81 +1,72 @@
-import * as React from 'react';
-import { Button, View, Text, ImageBackground, StyleSheet} from 'react-native';
-
+import * as React from "react";
+import {
+  TouchableOpacity,
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+} from "react-native";
 
 export default function LandingScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={styles.container}>
       <ImageBackground
         source={require("../../../assets/main-logo.png")}
         style={styles.logo}
       ></ImageBackground>
-      <Text>Landing Screen</Text>
-      <Text>Temuin lahir dari sebuah masalah yang dialami berbagai kalangan masyarakat. Kehilangan sesuatu merupakan hal yang tidak diharapkan oleh siapapun. Seringkali kita tidak begitu saja mengikhlaskan apa yang telah hilang, namun mencoba untuk berikhtiar dalam menemukan kembali. Tak jarang kita jumpai postingan di berbagai sosmed seperti status WhatsApp, cerita Instagram, grup facebook, dan sebagainya mengenai barang maupun hewan peliharaan yang hilang. Tiang listrik dan tembok sebuah bangunan juga tak luput menjadi sasaran untuk menempel poster pengumuman kehilangan.
+      <Text style={styles.wellcomeText}>Wellcome to Temuin
       </Text>
-      <Text>Disini nantinya akan mengarahkan ke Login / register jika belum login</Text>
-
-      <Button
-        title="Mulai"
-        onPress={() => navigation.navigate('Login')}
-      />
+      <Text style={styles.descripton}>Netizen Temuin siap bantu cari barang mu yang hilang
+      </Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={styles.text}>Mulai</Text>
+        </TouchableOpacity>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 100,
+    paddingTop: 120,
     alignItems: "center",
   },
   logo: {
     width: 200,
     height: 185,
   },
-  welcomeText: {
-    fontWeight: "bold",
-    fontSize: 35,
-    paddingTop: 20,
-  },
-  loginText: {
-    fontSize: 15,
-    paddingTop: 10,
-  },
-  username: {
-    height: 40,
-    width: 300,
-    marginTop: 50,
-    margin: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 20,
-  },
-  password: {
-    height: 40,
-    width: 300,
-    margin: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderRadius: 20,
-  },
   button: {
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 50,
     width: 150,
-    height: 40,
+    height: 60,
     elevation: 3,
-    backgroundColor: "black",
+    backgroundColor: "#E1CBCF",
     marginTop: 10,
   },
   text: {
-    fontSize: 16,
-    lineHeight: 21,
+    fontSize: 20,
     fontWeight: "bold",
-    letterSpacing: 0.25,
+    letterSpacing: 1,
     color: "white",
   },
-  signin: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginTop: 10,
+  descripton:{
+    fontSize: 20,
+    letterSpacing: 0.25,
+    color: "#8A4065",
+    textAlign:'center',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 40,
+    marginBottom: 40,
   },
+  wellcomeText :{
+    fontWeight: "bold",
+    color: "#8A4065",
+    fontSize: 40,
+    marginTop: 50,
+
+  }
 });
