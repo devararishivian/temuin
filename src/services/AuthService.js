6e1d0cba934966a52052b0b42020e573130814f6
Devvara Rishivian <rishivian@gmail.com>
2022-10-22 22:35:02 +0700

    feat: check if there is error

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -6,39 +6,9 @@ export async function register(requestBody) {
         errorMessage: null,
     };
 
-    const { isError, errorMessage, createdUserID } = await registerToSupabase(requestBody.email, requestBody.password);
-    if (isError) {
-        response.isError = true;
-        response.errorMessage = errorMessage;
-
-        return response;
-    }
-
-    const userDataToInsert = {
-        id: createdUserID,
-        username: requestBody.username,
-        name: requestBody.name
-    };
-
-    const { isError: insertUserDataErr, errorMessage: insertUserDataErrMsg } = await insertUserData(userDataToInsert);
-    if (insertUserDataErr) {
-        response.isError = true;
-        response.errorMessage = insertUserDataErrMsg;
-
-        return response;
-    }
-}
-
-async function registerToSupabase(email, password) {
-    let response = {
-        isError: false,
-        errorMessage: null,
-        createdUserID: null
-    };
-
     const { data, error } = await supabase.auth.signUp({
-        email: email,
-        password: password
+        email: requestBody.email,
+        password: requestBody.password
     })
 
     if (error) {
@@ -48,32 +18,31 @@ async function registerToSupabase(email, password) {
         return response;
     }
 
-    response.createdUserID = data.user.id;
     return response;
 }
 
-async function insertUserData(requestBody) {
-    let response = {
-        isError: false,
-        errorMessage: null
-    };
+// async function insertUserData(requestBody) {
+//     let response = {
+//         isError: false,
+//         errorMessage: null
+//     };
 
-    const { data, error } = await supabase
-        .from('user')
-        .insert([
-            {
-                id: requestBody.id,
-                username: requestBody.username,
-                name: requestBody.name
-            },
-        ]);
+//     const { data, error } = await supabase
+//         .from('user')
+//         .insert([
+//             {
+//                 id: requestBody.id,
+//                 username: requestBody.username,
+//                 name: requestBody.name
+//             },
+//         ]);
 
-    if (error) {
-        response.isError = true;
-        response.errorMessage = error.message;
+//     if (error) {
+//         response.isError = true;
+//         response.errorMessage = error.message;
 
-        return response;
-    }
+//         return response;
+//     }
 
-    return response;
-}
+//     return response;
+// }
