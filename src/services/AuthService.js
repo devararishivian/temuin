dea405180aece4711910fbfe7145471f95336b0c
Devvara Rishivian <rishivian@gmail.com>
2022-10-22 18:28:48 +0700

    save

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -5,11 +5,25 @@ export async function register(requestBody) {
         isError: false,
         errorMessage: null,
     };
-    
+
     const { isError, errorMessage, createdUserID } = await registerToSupabase(requestBody.email, requestBody.password);
     if (isError) {
         response.isError = true;
         response.errorMessage = errorMessage;
+
+        return response;
+    }
+
+    const userDataToInsert = {
+        id: createdUserID,
+        username: requestBody.username,
+        name: requestBody.name
+    };
+
+    const { isError: insertUserDataErr, errorMessage: insertUserDataErrMsg } = await insertUserData(userDataToInsert);
+    if (insertUserDataErr) {
+        response.isError = true;
+        response.errorMessage = insertUserDataErrMsg;
 
         return response;
     }
@@ -35,5 +49,31 @@ async function registerToSupabase(email, password) {
     }
 
     response.createdUserID = data.user.id;
+    return response;
+}
+
+async function insertUserData(requestBody) {
+    let response = {
+        isError: false,
+        errorMessage: null
+    };
+
+    const { data, error } = await supabase
+        .from('user')
+        .insert([
+            {
+                id: requestBody.id,
+                username: requestBody.username,
+                name: requestBody.name
+            },
+        ]);
+
+    if (error) {
+        response.isError = true;
+        response.errorMessage = error.message;
+
+        return response;
+    }
+
     return response;
 }
