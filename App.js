75dc2304a06ea935db97c8cee748974e79e6d858
Devvara Rishivian <rishivian@gmail.com>
2022-10-28 19:35:55 +0700

    chore: update dependencies

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import BottomNavigator from './src/components/BottomNavigator';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import useAuthStore from './src/store/index'
+import useAuthStore from './src/store/index';
 import LandingScreen from './src/screens/Auth/Landing';
 import LoginScreen from './src/screens/Auth/Login';
 import RegisterScreen from './src/screens/Auth/Register';
@@ -18,7 +18,7 @@ const AuthScreen = () => {
       <Stack.Screen name="Landing" component={LandingScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
-      <Stack.Screen name="NewPost" component={NewPostScreen}/>
+      <Stack.Screen name="NewPost" component={NewPostScreen} />
     </Stack.Navigator>
   );
 }
